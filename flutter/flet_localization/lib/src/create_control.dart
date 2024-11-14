5d08926d070cc5b33e43fdfbd2dc448b7550615c
tct123 <42028373+tct123@users.noreply.github.com>
2024-11-14 19:31:10 +0100

    Update create_control.dart

--- flutter/flet_localization/lib/src/create_control.dart
+++ flutter/flet_localization/lib/src/create_control.dart
@@ -1,5 +1,6 @@
 import 'package:flet/flet.dart';
 import "package:mylocalization/mylocalization.dart";
+
 CreateControlFactory createControl = (CreateControlArgs args) {
   switch (args.control.type) {
     default:
